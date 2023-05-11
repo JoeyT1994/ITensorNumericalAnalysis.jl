3034ea54121f56e95c48506aa2f8b9554dbfb6c8
Joseph Tindall <joeytindall94@googlemail.com>
2023-05-11 14:02:21 -0400

    Added Support for calculating bitstrings

---
+++ QTT_utils.jl
@@ -0,0 +1,21 @@
+include("itensornetwork_functions.jl")
+
+function calculate_x(xis::Dict, vertex_map::Dict; a::Float64 = 1.0)
+    @assert keys(vertex_map) == keys(xis)
+    return sum([a*xis[v]/(2^vertex_map[v]) for v in keys(xis)])
+end
+
+function calculate_xis(x::Float64, vertex_map::Dict; a::Float64 = 1.0)
+    x_rn = x/a
+    xis = Dict()
+    sorted_vertex_map = sort(vertex_map; byvalue = true)
+    for v in keys(sorted_vertex_map)
+        if(x_rn >= 1.0 / (2^vertex_map[v]))
+            xis[v] = 1
+            x_rn -= 1.0 / (2^vertex_map[v])
+        else
+            xis[v] = 0
+        end
+    end
+    return xis
+end
