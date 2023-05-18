900eac10b9e1d8f3fb51320e7557f8bafb7f93a3
Joey <joeytindall94@googlemail.com>
2023-05-18 16:32:40 -0400

    Added QTTITensorNetworks.jl back for testing

---
+++ src/QTTITensorNetworks.jl
@@ -0,0 +1,10 @@
+using ITensors
+using ITensorNetworks
+using NamedGraphs
+using EllipsisNotation
+using Graphs
+
+using ITensorNetworks: delta_network
+using NamedGraphs: add_edges
+
+include("QTT_utils.jl")
