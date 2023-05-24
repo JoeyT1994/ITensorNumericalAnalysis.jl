bef23e5143951650d996db2f8b8ed8439ed6efb8
Ryan Levy <rlevy@flatironinstitute.org>
2023-05-24 15:58:13 -0400

    include shift

--- examples/polynomial.jl
+++ examples/polynomial.jl
@@ -23,9 +23,10 @@ function main()
   g = named_grid((L, 1))
   s = siteinds("S=1/2", g)
 
-  # construct f(x) = ax^n
+  # construct f(x) = a(x-shift)^n 
   n = 2 # degree
-  a = 1.0
+  a = 0.5
+  shift = -0.5
 
   #Define a map which determines a canonical ordering of the vertices of the network
   vertex_map = Dict(vertices(g) .=> [i for i in 1:L])
@@ -36,33 +37,32 @@ function main()
     xi = 1.0 / 2^vertex_map[v]
     if (vertex_map[v] in [1]) # left edge
       vec = zeros((1, n + 1))
-      vec[end] = 1
-      ψ[v][1, :] = vec
-
-      for i in 1:(n + 1)
-        vec[i] = sqrt(binomial(n, n - (i - 1))) * xi^(n - (i - 1))
+      for (p, val) in [(1, shift / L), (2, xi + shift / L)]
+        for i in 1:(n + 1)
+          vec[i] = sqrt(binomial(n, n - (i - 1))) * val^(n - (i - 1))
+        end
+        ψ[v][p, :] = vec
       end
-      ψ[v][2, :] = vec
     elseif vertex_map[v] in [L] # right edge
       vec = zeros((1, n + 1))
-      vec[1] = 1
-      ψ[v][1, :] = vec
-
-      for i in 1:(n + 1)
-        vec[end - i + 1] = sqrt(binomial(n, n - (i - 1))) * xi^(n - (i - 1))
+      for (p, val) in [(1, shift / L), (2, xi + shift / L)]
+        for i in 1:(n + 1)
+          vec[end - i + 1] = sqrt(binomial(n, n - (i - 1))) * val^(n - (i - 1))
+        end
+        ψ[v][p, :] = vec
       end
-      ψ[v][2, :] = vec
 
     else # bulk
       mat = Matrix{Float64}(I, n + 1, n + 1)
-      ψ[v][1, :, :] = mat
-      for i in 2:(n + 1)
-        for j in 1:(i - 1)
-          c = coeff(n, i, j)
-          mat[i, j] = c * xi^(i - j)
+      for (p, val) in [(1, shift / L), (2, xi + shift / L)]
+        for i in 2:(n + 1)
+          for j in 1:(i - 1)
+            c = coeff(n, i, j)
+            mat[i, j] = c * val^(i - j)
+          end
         end
+        ψ[v][p, :, :] = mat
       end
-      ψ[v][2, :, :] = mat
     end
   end
   ψ[first(vertices(ψ))] *= a
@@ -77,7 +77,7 @@ function main()
     push!(values, ITensors.contract(ψ12proj)[])
   end
 
-  @show (a * valid_points .^ n) ≈ values
+  @show (a * (valid_points .+ shift) .^ n) ≈ values
 end
 
 main()
