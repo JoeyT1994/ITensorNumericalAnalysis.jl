17b21891b4374ea27eee470abf48881a8e1a3f60
Ryan Levy <rlevy@flatironinstitute.org>
2023-05-25 17:08:05 -0400

    Clean up code

--- examples/polynomial_full.jl
+++ examples/polynomial_full.jl
@@ -10,14 +10,6 @@ using NamedGraphs: add_edges
 
 include("../src/QTT_utils.jl")
 
-function coeff(n, r, c)
-  @assert r ≥ c
-  # make this more efficient later
-  multi = factorial(n) / (factorial(r - c) * factorial(c - 1) * factorial(n + 1 - r))
-
-  return multi * 1 / sqrt(binomial(n, n + 1 - r)) * 1 / sqrt(binomial(n, c - 1))
-end
-
 function main()
   L = 10
   g = named_grid((L, 1))
@@ -25,7 +17,8 @@ function main()
 
   # polynomial degree, i.e. f(x) = ∑ᵢⁿ cᵢ xⁱ 
   coeffs = [-0.021, 0.31, -1.1, 1]
-  # coeffs will build f(x) = 1 -x + 0.5x^2
+  # coeffs will build f(x) = (x-0.1)(x-0.3)(x-0.7)
+  #                        = -0.021 + 0.31 x - 1.1 x^2 + x^3
   n = length(coeffs) - 1
 
   #Define a map which determines a canonical ordering of the vertices of the network
@@ -33,41 +26,27 @@ function main()
 
   ψ = delta_network(s; link_space=n + 1)
   for v in vertices(ψ)
-    ψ[v] = ITensor(Float64, inds(ψ[v]))
+    ψ[v] = ITensor(0.0, inds(ψ[v]))
     xi = 1.0 / 2^vertex_map[v]
     if (vertex_map[v] in [1]) # left edge
       # left boundary has onehot(left_link => 1)
       # so identity on 1, 1/2^i on 2
-      vec = zeros((2, n + 1))
-      vec[1, 1] = 1
-      for i in 1:(n + 1)
-        vec[2, i] = xi^(i - 1)
-      end
-      ψ[v][:, :] = vec
+      ψ[v][1, 1] = 1
+      ψ[v][2, :] = [xi^(i - 1) for i in 1:(n + 1)]
     elseif vertex_map[v] in [L] # right edge
       # right boundary multiplies in the coefficients
-      fake = Index(n + 1, "fake")
-      temp_right = ITensor(Float64, inds(ψ[v]), fake)
-      mat = Matrix{Float64}(I, n + 1, n + 1)
-      temp_right[1, :, :] = mat
-      for β in 2:(n + 1)
-        for α in 1:(β - 1)
-          c = binomial(β - 1, α - 1)
-          mat[α, β] = c * xi^(β - α)
-        end
-      end
-      temp_right[2, :, :] = mat
-      ψ[v] = temp_right * ITensor(coeffs, fake)
+      dummy = Index(n + 1, "dummy")
+      temp_right = ITensor(Float64, inds(ψ[v]), dummy)
+      temp_right[1, :, :] = Matrix{Float64}(I, n + 1, n + 1)
+      temp_right[2, :, :] = [
+        (α ≤ β) ? binomial(β - 1, α - 1) * xi^(β - α) : 0 for α in 1:(n + 1), β in 1:(n + 1)
+      ]
+      ψ[v] = temp_right * ITensor(coeffs, dummy)
     else # bulk
-      mat = Matrix{Float64}(I, n + 1, n + 1)
-      ψ[v][1, :, :] = mat
-      for β in 2:(n + 1)
-        for α in 1:(β - 1)
-          c = binomial(β - 1, α - 1)
-          mat[α, β] = c * xi^(β - α)
-        end
-      end
-      ψ[v][2, :, :] = mat
+      ψ[v][1, :, :] = Matrix{Float64}(I, n + 1, n + 1)
+      ψ[v][2, :, :] = [
+        (α ≤ β) ? binomial(β - 1, α - 1) * xi^(β - α) : 0 for α in 1:(n + 1), β in 1:(n + 1)
+      ]
     end
   end
 
@@ -85,6 +64,7 @@ function main()
   for (i, c) in enumerate(coeffs)
     ans .+= c * (valid_points) .^ (i - 1)
   end
+
   @show ans ≈ values
 end
 
