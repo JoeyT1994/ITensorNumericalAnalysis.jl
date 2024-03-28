4caa5b2b64eb6d697dfd71ba905d673c54c3b9c8
Joey <joeytindall94@googlemail.com>
2024-03-28 18:17:00 -0400

    formating

--- src/itensornetworksutils.jl
+++ src/itensornetworksutils.jl
@@ -1,22 +1,3 @@
-#Imports
-using ITensors
-using Graphs
-using NamedGraphs
-using ITensorNetworks
-using EllipsisNotation
-using Dictionaries
-using DataGraphs
-
-using ITensorNetworks: delta_network, data_graph, data_graph_type
-using NamedGraphs: add_edges, random_bfs_tree, rem_edges
-
-using SplitApplyCombine
-
-using Random
-using Distributions
-
-include("itensornetworks_elementary_functions.jl")
-
 """Build the order L tensor corresponding to fx(x): x ∈ [0,1]."""
 function build_full_rank_tensor(L::Int64, fx::Function)
   inds = [Index(2, "$i") for i in 1:L]
@@ -47,11 +28,24 @@ function c_tensor(phys_ind::Index, virt_inds::Vector)
 end
 
 function copy_tensor_network(s::IndsNetwork, linkdim::Int64)
-  tn = randomITensorNetwork(s; link_space = linkdim)
+  tn = randomITensorNetwork(s; link_space=linkdim)
   for v in vertices(tn)
     virt_inds = setdiff(inds(tn[v]), Index[only(s[v])])
     tn[v] = c_tensor(only(s[v]), virt_inds)
   end
 
   return tn
+end
+
+function operate(
+  operator::AbstractITensorNetwork,
+  ψ::ITensorNetworkFunction;
+  truncate_kwargs=(;),
+  kwargs...,
+)
+  ψ_tn = TTN(itensornetwork(ψ))
+  ψO_tn = noprime(ITensors.contract(operator, ψ_tn; init=prime(copy(ψ_tn)), kwargs...))
+  ψO_tn = truncate(ψO_tn; truncate_kwargs...)
+
+  return ITensorNetworkFunction(ITensorNetwork(ψO_tn), bit_map(ψ))
 end
